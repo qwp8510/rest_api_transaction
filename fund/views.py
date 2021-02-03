92398cafc341b6421efc0e55855725cb05876167
Daniel Chiang <44150378+qwp8510@users.noreply.github.com>
2021-02-03 16:15:40 +0800

    add basic auth token authorization and add permission into fund view

--- fund/views.py
+++ fund/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework import permissions
 
 from fund.models import Targets
 from fund.serializers import TargetsSerializer
 
+
 class TargetsViewSet(viewsets.ModelViewSet):
     queryset = Targets.objects.all()
     serializer_class = TargetsSerializer
+    permission_classes = [permissions.DjangoModelPermissions]
