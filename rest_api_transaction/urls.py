2a9d771febdbcc0c49dea3db355495df5383b671
Daniel Chiang <44150378+qwp8510@users.noreply.github.com>
2021-02-07 02:29:14 +0800

    add receivers url

--- rest_api_transaction/urls.py
+++ rest_api_transaction/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('fund.urls')),
+    path('', include('receivers.urls')),
     path('api/', include('djoser.urls.authtoken')),
 ]
